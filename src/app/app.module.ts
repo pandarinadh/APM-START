140b96b2129ea3b205350016467868c4decbb73a
Pandari B <pandarinadh@yahoo.com>
2019-04-05 13:03:09 -0400

    Module changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,35 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
-import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
-import { StarComponent } from './shared/star.component';
+
 import { HttpClientModule } from '@angular/common/http';
 import { WelcomeComponent } from './home/welcome.component';
-import { ProductDetailComponent } from './products/product-detail.component'
+
+import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    WelcomeComponent,
-    ProductDetailComponent
+    WelcomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    RouterModule.forRoot([
-        {path: 'products', component: ProductListComponent},
-        {path: 'products/:id', component: ProductDetailComponent},
-        {path: 'welcome', component: WelcomeComponent},
-        {path: '**', redirectTo: 'welcome', pathMatch: 'full'}
-    ])
+    ProductModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
